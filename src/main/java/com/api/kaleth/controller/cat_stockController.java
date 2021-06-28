cbe9fc0c6b3f8fc27ba7669f862c962fca3708c3
Jayli9 <jayli_97@hotmail.es>
2021-06-28 17:51:55 -0500

    edit view home products and list paginator

--- src/main/java/com/api/kaleth/controller/cat_stockController.java
+++ src/main/java/com/api/kaleth/controller/cat_stockController.java
@@ -26,20 +26,20 @@ public class cat_stockController {
 	@Autowired
 	cat_stockRepository cat_stockRepository;
 
-
 	@GetMapping("/stock")
-	public List<CatStock> getCatDiseno(){
+	public List<CatStock> getCatDiseno() {
 		List<CatStock> CatStock = cat_stockRepository.findAll();
 		return CatStock;
-		
+
 	}
-	
+
 	@GetMapping("/stock/{id}")
-	public Optional<CatStock> getStock(@PathVariable Long id) throws ResourceNotFoundException{
+	public Optional<CatStock> getStock(@PathVariable Long id) throws ResourceNotFoundException {
 		Optional<CatStock> OneStock = cat_stockRepository.findById(id);
-		
+
 		return OneStock;
 	}
+
 	@PostMapping("/stock")
 	public CatStock createCatStock(@RequestBody CatStock CatStock) {
 
@@ -47,45 +47,46 @@ public class cat_stockController {
 
 		return newStock;
 	}
-	
-	@RequestMapping(value="/stock/number/{id_producto}/{id_puntoventa}",produces = {"application/json"},method= RequestMethod.GET)
-	public int encontrarStock(@PathVariable("id_producto") Integer id_producto, @PathVariable("id_puntoventa") Integer id_puntoventa) {
-		int numeroTipo = 0;	
+
+	@RequestMapping(value = "/stock/number/{id_producto}/{id_puntoventa}", produces = {
+			"application/json" }, method = RequestMethod.GET)
+	public int encontrarStock(@PathVariable("id_producto") Integer id_producto,
+			@PathVariable("id_puntoventa") Integer id_puntoventa) {
+		int numeroTipo = 0;
 		try {
-			 numeroTipo= cat_stockRepository.encontrarStock(id_producto, id_puntoventa);
-			 System.out.println(numeroTipo);
-			 
-			 return numeroTipo;
+			numeroTipo = cat_stockRepository.encontrarStock(id_producto, id_puntoventa);
+			System.out.println(numeroTipo);
+
+			return numeroTipo;
 		} catch (Exception e) {
 			System.out.println("*************************ERRRORRRRRRRRRRRRRRRRRRR");
 		}
 		return -1;
 	}
-	
-	@RequestMapping(value="/stock/updateRest/{id_producto}/{id_puntoventa}/{cantidad}",produces = {"application/json"},method= RequestMethod.GET)
-	public int UpdateStock(@PathVariable("id_producto") Integer id_producto, 
-			@PathVariable("id_puntoventa") Integer id_puntoventa, 
-			@PathVariable("cantidad") Integer cantidad) {
-		int actualizado = 0;	
+
+	@RequestMapping(value = "/stock/updateRest/{id_producto}/{id_puntoventa}/{cantidad}", produces = {
+			"application/json" }, method = RequestMethod.GET)
+	public int UpdateStock(@PathVariable("id_producto") Integer id_producto,
+			@PathVariable("id_puntoventa") Integer id_puntoventa, @PathVariable("cantidad") Integer cantidad) {
+		int actualizado = 0;
 		try {
 			actualizado = cat_stockRepository.actualizarStockRestar(cantidad, id_producto, id_puntoventa);
-			 System.out.println(actualizado);
-			 
-			 return actualizado;
+			System.out.println(actualizado);
+
+			return actualizado;
 		} catch (Exception e) {
 			System.out.println("*************************ERRRORRRRRRRRRRRRRRRRRRR");
 		}
 		return -1;
 	}
 
-
-	
 	@PutMapping("/stock/{id}")
 	public ResponseEntity<String> updateStock(@RequestBody CatStock stock, @PathVariable Long id)
-		throws ResourceNotFoundException{
-		
-		CatStock findStock = cat_stockRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("No se encuentra el id"));
-	
+			throws ResourceNotFoundException {
+
+		CatStock findStock = cat_stockRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No se encuentra el id"));
+
 		findStock.setCantidad(stock.getCantidad());
 		findStock.setStockMax(stock.getStockMax());
 		findStock.setStockMin(stock.getStockMin());
@@ -93,78 +94,90 @@ public class cat_stockController {
 		findStock.setPrecioMayor(stock.getPrecioMayor());
 		findStock.setPrecioUnit(stock.getPrecioUnit());
 		findStock.setExiste(stock.getExiste());
-		
+
 		CatStock updateStock = cat_stockRepository.save(findStock);
 		return ResponseEntity.ok().header("Content-Type", "application/json")
 				.body("{\"mensaje\": \"El Stock se ha actualizado correctamente " + "" + "\"}");
 	}
+
 	@DeleteMapping("/stock/{id}")
-	public ResponseEntity<String> deleteStock(@PathVariable Long id)throws ResourceNotFoundException{
-		
-		CatStock findStock = cat_stockRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("No se encontro el id"));
+	public ResponseEntity<String> deleteStock(@PathVariable Long id) throws ResourceNotFoundException {
+
+		CatStock findStock = cat_stockRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No se encontro el id"));
 		cat_stockRepository.deleteById(id);
-		
+
 		return ResponseEntity.ok().header("Content-Type", "application/json")
 				.body("{\"mensaje\": \"El Stock se ha eliminado correctamente " + id + "\"}");
 	}
-	
-	
-	
-	
-	@RequestMapping(value="/stock/find/{id_producto}/{id_puntoventa}",produces = {"application/json"},method= RequestMethod.GET)
-	public List<CatStock> findStockIdProIdpPuntV (@PathVariable("id_producto") Integer idproducto, @PathVariable("id_puntoventa") Integer idpuntoventa) {
-		
+
+	@RequestMapping(value = "/stock/find/{id_producto}/{id_puntoventa}", produces = {
+			"application/json" }, method = RequestMethod.GET)
+	public List<CatStock> findStockIdProIdpPuntV(@PathVariable("id_producto") Integer idproducto,
+			@PathVariable("id_puntoventa") Integer idpuntoventa) {
+
 		try {
-			List<CatStock> productostock = cat_stockRepository.findbyIdProductoIdPuntosVenta(idproducto,idpuntoventa);
-			 System.out.println(productostock);
-			 
-			 return productostock;
+			List<CatStock> productostock = cat_stockRepository.findbyIdProductoIdPuntosVenta(idproducto, idpuntoventa);
+			System.out.println(productostock);
+
+			return productostock;
 		} catch (Exception e) {
 			System.out.println("*************************ERRRORRRRRRRRRRRRRRRRRRR");
 		}
 		return null;
 	}
-	@RequestMapping(value="/stock/updates/{id_producto}/{id_puntoventa}/{cantidad}/"
-			+ "{precioUnit}/{precioMayor}/{precioDist}/{stockMax}/{stockMin}",
-			produces = {"application/json"},method= RequestMethod.GET)
-	public int UpdateStocks(@PathVariable("id_producto") Integer id_producto, 
-			@PathVariable("id_puntoventa") Integer id_puntoventa, 
-			@PathVariable("cantidad") Integer cantidad,
-			@PathVariable("precioUnit") Float precioUnit,
-			@PathVariable("precioMayor") Float precioMayor,
-			@PathVariable("precioDist") Float precioDist,
-			@PathVariable("stockMax") Integer stockMax,
+
+	@RequestMapping(value = "/stock/updates/{id_producto}/{id_puntoventa}/{cantidad}/"
+			+ "{precioUnit}/{precioMayor}/{precioDist}/{stockMax}/{stockMin}", produces = {
+					"application/json" }, method = RequestMethod.GET)
+	public int UpdateStocks(@PathVariable("id_producto") Integer id_producto,
+			@PathVariable("id_puntoventa") Integer id_puntoventa, @PathVariable("cantidad") Integer cantidad,
+			@PathVariable("precioUnit") Float precioUnit, @PathVariable("precioMayor") Float precioMayor,
+			@PathVariable("precioDist") Float precioDist, @PathVariable("stockMax") Integer stockMax,
 			@PathVariable("stockMin") Integer stockMin) {
-		int actualizado = 0;	
-		
+		int actualizado = 0;
+
 		try {
-			actualizado = 
-			cat_stockRepository.actualizarStocks(cantidad, 
-					precioUnit,
-					precioMayor,
-					precioDist,
-					stockMax,
-					stockMin,	
-					id_producto, 
-					id_puntoventa);
-			 System.out.println(actualizado);
-			 	return actualizado;
+			actualizado = cat_stockRepository.actualizarStocks(cantidad, precioUnit, precioMayor, precioDist, stockMax,
+					stockMin, id_producto, id_puntoventa);
+			System.out.println(actualizado);
+			return actualizado;
 		} catch (Exception e) {
 			System.out.println("*************************ERRRORRRRRRRRRRRRRRRRRRR");
 		}
 		return -1;
 	}
-	
-	@RequestMapping(value="/stock/product/{codproducto}/{idpuntoventa}",produces = {"application/json"},method= RequestMethod.GET)
-	public List<CatStock> encontrarproductoStockby_codprod(@PathVariable("codproducto") Integer codproducto, @PathVariable("idpuntoventa") Integer idpuntoventa){
-		
+
+	@RequestMapping(value = "/stock/product/{codproducto}/{idpuntoventa}", produces = {
+			"application/json" }, method = RequestMethod.GET)
+	public List<CatStock> encontrarproductoStockby_codprod(@PathVariable("codproducto") Integer codproducto,
+			@PathVariable("idpuntoventa") Integer idpuntoventa) {
+
 		try {
-			List<CatStock> productoStock =  cat_stockRepository.findProductoStockby_codProducto(codproducto, idpuntoventa);
-			
+			List<CatStock> productoStock = cat_stockRepository.findProductoStockby_codProducto(codproducto,
+					idpuntoventa);
+
 			return productoStock;
-		}catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println("*************************ERRRORRRRRRRRRRRRRRRRRRR encontrarproductoStockby_codprod");
 		}
 		return null;
 	}
+
+	@RequestMapping(value = "/stock/exist/{incio}/{numeroFilas}", produces = {
+			"application/json" }, method = RequestMethod.GET)
+	public List<CatStock> getStockAllExist(@PathVariable("incio") Integer inicio,
+			@PathVariable("numeroFilas") Integer numeroFilas) {
+		List<CatStock> CatStock = cat_stockRepository.findAllExitents(inicio, numeroFilas);
+		return CatStock;
+
+	}
+
+	@RequestMapping(value = "/stock/cant", produces = { "application/json" }, method = RequestMethod.GET)
+	public int getStockAllExist() {
+		int numeroExistentes = cat_stockRepository.findCantidadAllExitents();
+		return numeroExistentes;
+
+	}
+
 }
--- src/main/java/com/api/kaleth/respository/cat_stockRepository.java
+++ src/main/java/com/api/kaleth/respository/cat_stockRepository.java
@@ -9,33 +9,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.api.kaleth.domain.CatStock;
 
-
 @RestController
 @RequestMapping("/api")
-public interface cat_stockRepository extends JpaRepository<CatStock, Long>{
+public interface cat_stockRepository extends JpaRepository<CatStock, Long> {
 
-	@Query(value="SELECT cantidad FROM cat_stock WHERE id_productos=? and id_puntos_venta=?", nativeQuery = true)
-	int encontrarStock(Integer id_producto,Integer id_puntoventa);
-	
-	@Query(value="UPDATE cat_stock SET cantidad=? WHERE id_productos=? and id_puntos_venta=?", nativeQuery = true)
-	int actualizarStockRestar(Integer cantidad_actualizada, Integer id_producto,Integer id_puntoventa);
-	
-	
-	@Query(value="UPDATE cat_stock SET cantidad=?, precio_unit=?, precio_mayor=?, precio_distribuidor=?, stock_max=?, stock_min=? WHERE id_productos=? and id_puntos_venta=?", nativeQuery = true)
-	int actualizarStocks(Integer cantidad_actualizada, 
-			Float precioUnit,
-			Float precioMay,
-			Float precioDist,
-			Integer stockMax,
-			Integer stockMin,
-			Integer id_producto,
-			Integer id_puntoventa);
-	
-	@Query(value="SELECT * FROM cat_stock s WHERE s.id_productos=? AND s.id_puntos_venta=?",nativeQuery = true)
-	List<CatStock> findbyIdProductoIdPuntosVenta(Integer id_producto,Integer id_puntoventa);
+	@Query(value = "SELECT cantidad FROM cat_stock WHERE id_productos=? and id_puntos_venta=?", nativeQuery = true)
+	int encontrarStock(Integer id_producto, Integer id_puntoventa);
 
-	@Query(value="SELECT * FROM cat_stock s , cat_productos p   WHERE p.cod_producto=? AND s.id_puntos_venta=? AND s.id_productos=p.id_productos AND existe='S'",nativeQuery = true)
+	@Query(value = "UPDATE cat_stock SET cantidad=? WHERE id_productos=? and id_puntos_venta=?", nativeQuery = true)
+	int actualizarStockRestar(Integer cantidad_actualizada, Integer id_producto, Integer id_puntoventa);
+
+	@Query(value = "UPDATE cat_stock SET cantidad=?, precio_unit=?, precio_mayor=?, precio_distribuidor=?, stock_max=?, stock_min=? WHERE id_productos=? and id_puntos_venta=?", nativeQuery = true)
+	int actualizarStocks(Integer cantidad_actualizada, Float precioUnit, Float precioMay, Float precioDist,
+			Integer stockMax, Integer stockMin, Integer id_producto, Integer id_puntoventa);
+
+	@Query(value = "SELECT * FROM cat_stock s WHERE s.id_productos=? AND s.id_puntos_venta=?", nativeQuery = true)
+	List<CatStock> findbyIdProductoIdPuntosVenta(Integer id_producto, Integer id_puntoventa);
+
+	@Query(value = "SELECT * FROM cat_stock s , cat_productos p   WHERE p.cod_producto=? AND s.id_puntos_venta=? AND s.id_productos=p.id_productos AND existe='S'", nativeQuery = true)
 	List<CatStock> findProductoStockby_codProducto(Integer codigo_producto, Integer id_puntoventa);
-	
+
+	@Query(value = "SELECT * FROM cat_stock s  WHERE s.existe='S'  LIMIT ?,?", nativeQuery = true)
+	List<CatStock> findAllExitents(Integer incio, Integer numeroFilas);
+
+	@Query(value = "SELECT COUNT(*) FROM cat_stock s  WHERE s.existe='S' ", nativeQuery = true)
+	int findCantidadAllExitents();
 
 }
